66373f99d0644f002db7b1a7c70443e5bc5e722e
Franco Silva <franco_tigres18@hotmail.com>
2023-06-20 17:55:54 -0600

    Proyecto Inicializado

---
+++ src/components/Formulario.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { reactive } from "vue";
+
+const paises = [
+  { codigo: "US", nombre: "Estados Unidos" },
+  { codigo: "MX", nombre: "México" },
+  { codigo: "AR", nombre: "Argentina" },
+  { codigo: "CO", nombre: "Colombia" },
+  { codigo: "CR", nombre: "Costa Rica" },
+  { codigo: "ES", nombre: "España" },
+  { codigo: "PE", nombre: "Perú" },
+];
+
+const busqueda = reactive({
+  ciudad: "",
+  pais: "",
+});
+</script>
+
+<template>
+  <form action="" class="formulario">
+    <div class="campo">
+      <label for="ciudad">Ciudad:</label>
+      <input
+        type="text"
+        id="ciudad"
+        placeholder="Ciudad"
+        v-model="busqueda.ciudad"
+      />
+    </div>
+
+    <div class="campo">
+      <label for="pais">País:</label>
+      <select id="pais" v-model="busqueda.pais">
+        <option value="">-- Seleccione un país --</option>
+        <option v-for="pais in paises" :value="pais.codigo">
+          {{ pais.nombre }}
+        </option>
+      </select>
+    </div>
+
+    <input type="submit" value="Consultar Clima" />
+  </form>
+</template>
